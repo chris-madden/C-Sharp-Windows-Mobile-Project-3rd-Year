ffaed45e166cb0fc2c3cd029a35c69ed5d9ae404
Christy Madden <christymadden80@gmail.com>
2016-03-19 13:29:16 +0000

    Accelerometer controls the ships movement on the x-axis

--- Assets/Scripts/Accelerometer_Movement.cs
+++ Assets/Scripts/Accelerometer_Movement.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class Accelerometer_Movement : MonoBehaviour {
 
+    // Controls the speed of the ship movement
+    public float speed = 5;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+        // Position ship on start up to bottom center of screen 
+        transform.position = new Vector2(0, -3);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        // Accelerometer control on X axis
+        transform.Translate(Vector3.right * Input.acceleration.x / speed, Space.World);
+
+    }
 }
