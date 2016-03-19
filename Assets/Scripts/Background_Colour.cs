3315cea2cbeef14520d7eac3b4b929e718297c1f
Christy Madden <christymadden80@gmail.com>
2016-03-19 14:51:31 +0000

    Added stars to scene to give perception of speed and black background has been added to scene also

---
+++ Assets/Scripts/Background_Colour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background_Colour : MonoBehaviour {
+
+    public Renderer background;
+
+	// Use this for initialization
+	void Start () {
+
+        background = GetComponent<Renderer>();
+
+        background.material.color = Color.black;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
