1d2592642a6cc89302bda4761ae432cd31475902
Christy Madden <christymadden80@gmail.com>
2016-03-19 14:12:56 +0000

    Boudaries added to game, ship can't go past left and right side of screen

--- Assets/Scripts/Accelerometer_Movement.cs
+++ Assets/Scripts/Accelerometer_Movement.cs
@@ -6,19 +6,42 @@ public class Accelerometer_Movement : MonoBehaviour {
     // Controls the speed of the ship movement
     public float speed = 5;
 
-	// Use this for initialization
-	void Start () {
+    // Will contain the size of the ship
+    Vector3 shipSize;
+
+    // Use this for initialization
+    void Start () {
 
         // Position ship on start up to bottom center of screen 
         transform.position = new Vector2(0, -3);
 
-    }
+        // Get the size of the ship
+        shipSize = GetComponent<Renderer>().bounds.size;
+
+    }// End Start
 	
 	// Update is called once per frame
 	void Update () {
 
-        // Accelerometer control on X axis
-        transform.Translate(Vector3.right * Input.acceleration.x / speed, Space.World);
+        // Get the Z value
+        var distance = (transform.position.z - Camera.main.transform.position.z);
 
-    }
+        // Store ships position
+        var shipPos = transform.position;
+
+        // Store positon of left side of screen relative to the cameras view
+        var leftScreen = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).x + (shipSize.x / (float)1.5);
+
+        // Store positon of right side of screen relative to the cameras view
+        var rightScreen = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance)).x - (shipSize.x / (float)1.5);
+
+        // Clamp the ships position on the x axis so it can't go off screen
+        transform.position = new Vector3(Mathf.Clamp(shipPos.x, leftScreen, rightScreen), shipPos.y, shipPos.z);
+
+        //Accelerometer movement on x axis.
+        transform.Translate(Vector3.right * Input.acceleration.x, Space.World);
+
+
+    }// End update
+
 }
