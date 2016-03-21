e7be8bca3c42872f037ad9ab169ba2f51f801cc0
Christy Madden <christymadden80@gmail.com>
2016-03-21 12:00:46 +0000

    Missile will destroy off screen

--- Assets/Scripts/FireMissile.cs
+++ Assets/Scripts/FireMissile.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class FireMissile : MonoBehaviour {
 
+    // Represents the missile prefab
     public GameObject missile;
 
-    public Transform rocketPos;
+    // Time until missile is destroyed
+    public float destroyMissile = 2;
 
     // Use this for initialization
     void Start () {
@@ -16,13 +18,18 @@ public class FireMissile : MonoBehaviour {
 	void Update () {
 
        
-	
-	}
+
+    }
 
     public void fireMissile()
     {
         // Create missile on screen from center of position of ship and change it to type Rigidbody2D
-        Instantiate(missile, transform.position, Quaternion.identity);
-    }
+        // Store the instaniated missile into a variable called missileClone
+        var missileClone = Instantiate(missile, transform.position, Quaternion.identity);
+
+        // Destroy the missile clone after a set number of seconds
+        Destroy(missileClone, destroyMissile);
+
+    }// End fireMissile
 
 }
