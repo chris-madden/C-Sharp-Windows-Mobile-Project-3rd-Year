dd63f1a62e2bf440e214c37b7b37faff984f0f1f
Christy Madden <christymadden80@gmail.com>
2016-03-30 21:35:25 +0100

    Credits screen added

--- Assets/Scripts/Credits/Scroll_Credits.cs
+++ Assets/Scripts/Credits/Scroll_Credits.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Scroll_Credits : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private RectTransform credits;
+
+    public Text creditsText;
+
+    private float creditPos = -150;
+
+    private float speed = 2;
+
+    private float stopScrolling = 1800; 
+
+    // Use this for initialization
+    void Start () {
+
+        // Get reference to the Game titles Transform
+        credits = GetComponent<RectTransform>();
+
+        // Move the title above the screen
+        credits.anchoredPosition = new Vector2(0, creditPos);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        // Subtract from titlePos to move title down screen
+        creditPos += speed;
+
+        // If the title hasn't reached the center of the screen
+        if (creditPos < stopScrolling)
+        {
+            // Move the title down
+            credits.anchoredPosition = new Vector2(0, creditPos);
+        }
+
+    }
 }
--- Assets/Scripts/StartMenu/ErrorGUI.cs
+++ Assets/Scripts/StartMenu/ErrorGUI.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class ErrorGUI : MonoBehaviour {
 
-    bool buttonClicked = false;
-
     // Use this for initialization
     void Start () {
 	
